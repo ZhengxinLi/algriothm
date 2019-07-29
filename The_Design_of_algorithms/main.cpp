b4c8180bb18332f9ea77eef43f69d1cbdfba1c11
li <licominghahaha@gmail.com>
2019-07-29 19:52:24 +0800

    Initial Commit

---
+++ The_Design_of_algorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  The_Design_of_algorithms
+//
+//  Created by lizhengxin on 2019/7/29.
+//  Copyright © 2019 lizhengxin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
